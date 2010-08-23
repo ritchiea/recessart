e2dfa2e363e2053a58a81dc13b849d23b990c4f7
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-08-23 18:11:25 +0200

    update readme, seeds and add missing migration

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
-
 energy_program = Program.create :created_at => Time.now, :updated_at => Time.now, :name => 'Energy', :description => 'Energy'
 power_program = Program.create :created_at => Time.now, :updated_at => Time.now, :name => 'Power', :description => 'Power'
 coal_program = Program.create :created_at => Time.now, :updated_at => Time.now, :name => 'Coal', :description => 'Coal'
@@ -13,3 +5,71 @@ coal_program = Program.create :created_at => Time.now, :updated_at => Time.now, 
 Initiative.create :created_at => Time.now, :updated_at => Time.now, :name => 'Energy - initiative', :description => 'Energy - initiative', :program_id => energy_program
 Initiative.create :created_at => Time.now, :updated_at => Time.now, :name => 'Power - initiative', :description => 'Power - initiative', :program_id => power_program
 Initiative.create :created_at => Time.now, :updated_at => Time.now, :name => 'Coal - initiative', :description => 'Coal - initiative', :program_id => coal_program
+
+tax_class_group = MultiElementGroup.create :name => 'tax_classes', :description => 'TaxClass', :target_class_name => 'Organization'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => '509a1'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => '509a2'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => '509a3'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => 'Private Foundation'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => '501c4'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => '501c6'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => 'non-US'
+MultiElementValue.create :multi_element_group_id => tax_class_group.id, :value => 'Non-Exempt'
+
+expenditure_group = MultiElementGroup.create :name => 'expenditure_types', :description => 'ExpenditureType', :target_class_name => 'Request'
+MultiElementValue.create :multi_element_group_id => expenditure_group.id, :value => 'grant'
+MultiElementValue.create :multi_element_group_id => expenditure_group.id, :value => 'fip project'
+MultiElementValue.create :multi_element_group_id => expenditure_group.id, :value => 'fip consulting agreement'
+
+initiative_type_group = MultiElementGroup.create :name => 'initiative_types', :description => 'InitiativeType', :target_class_name => 'Request'
+MultiElementValue.create :multi_element_group_id => initiative_type_group.id, :value => 'Mature'
+MultiElementValue.create :multi_element_group_id => initiative_type_group.id, :value => 'Developing'
+MultiElementValue.create :multi_element_group_id => initiative_type_group.id, :value => 'Higher Risk'
+MultiElementValue.create :multi_element_group_id => initiative_type_group.id, :value => 'Former Allocation'
+
+constituents_group = MultiElementGroup.create :name => 'constituents', :description => 'Constituents', :target_class_name => 'Request'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Agriculture'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Business'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Consumer and Low Income'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Faith'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Military/Security'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Sportsman'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Environment'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Conservation/Lands'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Minority'
+MultiElementValue.create :multi_element_group_id => constituents_group.id, :value => 'Labor'
+
+means_usa_group = MultiElementGroup.create :name => 'usa_means', :description => 'MeansUsa', :target_class_name => 'Request'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Technical Analysis'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Economic Analysis'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Legal Intervention'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Regulatory Intervention'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Grassroots Organization'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Constituency Building'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Grasstops Organizing'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Media/PR'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Capacity Building'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Workshop/Conference'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Strategy Development'
+MultiElementValue.create :multi_element_group_id => means_usa_group.id, :value => 'Pilot Demonstration'
+
+means_china_group = MultiElementGroup.create :name => 'china_means', :description => 'MeansChina', :target_class_name => 'Request'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'Chinese Academic'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'Chinese Research Institute'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'China-based expert consultant'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'China-based NGO'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'International Academic'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'International Research Institute'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'International-based expert'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'consultant'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'International NGO'
+MultiElementValue.create :multi_element_group_id => means_china_group.id, :value => 'Other'
+
+user_salutation_group = MultiElementGroup.create :name => 'user_salutations', :description => 'UserSalutation', :target_class_name => 'User'
+MultiElementValue.create :multi_element_group_id => user_salutation_group.id, :value => 'Dr.'
+MultiElementValue.create :multi_element_group_id => user_salutation_group.id, :value => 'M.'
+MultiElementValue.create :multi_element_group_id => user_salutation_group.id, :value => 'Mr.'
+MultiElementValue.create :multi_element_group_id => user_salutation_group.id, :value => 'Ms.'
+MultiElementValue.create :multi_element_group_id => user_salutation_group.id, :value => 'Prof.'
+MultiElementValue.create :multi_element_group_id => user_salutation_group.id, :value => 'Rev.'
+    
