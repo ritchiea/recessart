a7414a2991cdb5d72df38c841b9997d43dd6bb0c
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-09-21 08:40:37 -0700

    update readme, make it so that login doesn't trigger audits, realtime updates or sphinx delta reindexing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,18 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
   include ::FluxxGrantUser
+  
+  
+  # Make it so that devise does not reindex delta indexing in sphinx upon signin
+  def update_tracked_fields_with_specific! request
+    User.without_auditing do
+      User.without_realtime do
+        User.suspended_delta(false) do
+          update_tracked_fields_without_specific! request
+        end
+      end
+    end
+  end
+  alias_method_chain :update_tracked_fields!, :specific
+  
 end
