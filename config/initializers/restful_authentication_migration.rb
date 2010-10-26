24fa506968c76124aa767f6244a7d526fca759e3
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-10-26 11:16:31 -0700

    remove restful authentication key

--- config/initializers/restful_authentication_migration.rb
+++
@@ -1,3 +0,0 @@
-# If you are migrating from a restful_authentication style DB, you can do this:
-REST_AUTH_SITE_KEY         = '0c66496cd33bf88117bd2a583ad213e0de755bb3'
-Authlogic::CryptoProviders::Sha1.instance_variable_set '@stretches', 10
