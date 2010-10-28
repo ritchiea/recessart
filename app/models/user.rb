f51dcbfe04862832498643cb8681eb63fdd97265
Eric Hansen <ehansen@eric-hansens-macbook-pro.local>
2010-10-28 09:33:05 -0700

    FLX-117 moving dashboard controller and routing to fluxx_engine plus a default view that should be overridden fix user creation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,13 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation
+  validates_confirmation_of :password
   acts_as_authentic do |c|
     # c.my_config_option = my_value # for available options see documentation in: Authlogic::ActsAsAuthentic
     c.act_like_restful_authentication = true
+    c.validate_login_field=false
+    c.validate_password_field=false
+    c.validate_email_field=false
   end # block optional
   
   include ::FluxxGrantUser
