f14365f92549e861ee44b5ff836815cec23d67b9
Eric Hansen <ehansen486@gmail.com>
2010-12-22 11:56:48 -0800

    SHAKTI-108 remove all test users from dropdowns

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101221000043) do
+ActiveRecord::Schema.define(:version => 20101222195611) do
 
   create_table "audits", :force => true do |t|
     t.datetime "created_at"
@@ -807,6 +807,7 @@ ActiveRecord::Schema.define(:version => 20101221000043) do
     t.string   "current_login_ip"
     t.string   "last_login_ip"
     t.integer  "user_profile_id"
+    t.boolean  "test_user_flag"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
