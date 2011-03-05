95b08e44e56ff969fcef1ae14c99c193b01cde08
Eric Hansen <ehansen486@gmail.com>
2011-03-04 20:40:51 -0800

    FLX-544 Loading a funding source down to sub-initiative causes no budget selection

--- db/migrate/20110305042739_fluxx_grant_add_prog_initiative_back_in_to_rfs.rb
+++ db/migrate/20110305042739_fluxx_grant_add_prog_initiative_back_in_to_rfs.rb
@@ -1,17 +1,17 @@
 class FluxxGrantAddProgInitiativeBackInToRfs < ActiveRecord::Migration
   def self.up
-    change_table :request_transactions do |t|
+    change_table :request_funding_sources do |t|
       t.integer :program_id, :sub_program_id, :initiative_id, :sub_initiative_id
     end
     
-    add_constraint 'request_transactions', 'request_transactions_program_id', 'program_id', 'programs', 'id'
-    add_constraint 'request_transactions', 'request_transactions_sub_program_id', 'sub_program_id', 'sub_programs', 'id'
-    add_constraint 'request_transactions', 'request_transactions_initiative_id', 'initiative_id', 'initiatives', 'id'
-    add_constraint 'request_transactions', 'request_transactions_sub_initiative_id', 'sub_initiative_id', 'sub_initiatives', 'id'
+    add_constraint 'request_funding_sources', 'request_funding_sources_program_id', 'program_id', 'programs', 'id'
+    add_constraint 'request_funding_sources', 'request_funding_sources_sub_program_id', 'sub_program_id', 'sub_programs', 'id'
+    add_constraint 'request_funding_sources', 'request_funding_sources_initiative_id', 'initiative_id', 'initiatives', 'id'
+    add_constraint 'request_funding_sources', 'request_funding_sources_sub_initiative_id', 'sub_initiative_id', 'sub_initiatives', 'id'
   end
 
   def self.down
-    change_table :request_transactions do |t|
+    change_table :request_funding_sources do |t|
       t.remove :program_id, :sub_program_id, :initiative_id, :sub_initiative_id
     end
   end
