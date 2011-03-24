bfdf135931ce053006dde2109d9f4cee96c67ee9
Andrew Lee <drew@intramedia.net>
2011-03-23 14:00:52 -1000

    FLX-214 Correct migration

--- db/migrate/20110323234336_fluxx_grant_add_grantee_user_profile_roles.rb
+++ db/migrate/20110323234336_fluxx_grant_add_grantee_user_profile_roles.rb
@@ -1,6 +1,6 @@
 class FluxxGrantAddGranteeUserProfileRoles < ActiveRecord::Migration
   def self.up
-    grantee_profile = UserProfile.where(:name => 'Grantee')
+    grantee_profile = UserProfile.where(:name => 'Grantee').first
     UserProfileRule.create :user_profile => grantee_profile, :permission_name => 'create_own', :model_type => 'Request'
     UserProfileRule.create :user_profile => grantee_profile, :permission_name => 'view_own', :model_type => 'Request'
     UserProfileRule.create :user_profile => grantee_profile, :permission_name => 'update_own', :model_type => 'Request'
@@ -12,7 +12,7 @@ class FluxxGrantAddGranteeUserProfileRoles < ActiveRecord::Migration
   end
 
   def self.down
-    grantee_profile = UserProfile.where(:name => 'Grantee')
+    grantee_profile = UserProfile.where(:name => 'Grantee').first
     UserProfileRule.where(:user_profile_id => grantee_profile.object_id, :permission_name => 'create_own', :model_type => 'Request').first.destroy
     UserProfileRule.where(:user_profile_id => grantee_profile.object_id, :permission_name => 'view_own', :model_type => 'Request').first.destroy
     UserProfileRule.where(:user_profile_id => grantee_profile.object_id, :permission_name => 'update_own', :model_type => 'Request').first.destroy
