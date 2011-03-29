7ace7492ce7c8029156bcb8301103fc40c1f4d00
Andrew Lee <drew@intramedia.net>
2011-03-29 12:32:34 -1000

    FLX-655 Request review table

--- db/migrate/20110329221122_fluxx_grant_create_request_review.rb
+++ db/migrate/20110329221122_fluxx_grant_create_request_review.rb
@@ -1,11 +1,11 @@
 class FluxxGrantCreateRequestReview < ActiveRecord::Migration
   def self.up
         create_table "request_reviews", :force => true do |t|
-    t.timestamps
-    t.integer :created_by_id, :updated_by_id, :request_id, :rating, :null => true, :limit => 12
-    t.string :review_type, :null => false, :default => 'RequestReport'
-    t.text :comment, :benefits, :outcomes, :merits
-    t.datetime :locked_until, :deleted_at, :null => true
+          t.timestamps
+          t.integer :created_by_id, :updated_by_id, :request_id, :rating, :null => true, :limit => 12
+          t.string :review_type, :null => true
+          t.text :comment, :benefits, :outcomes, :merits, :recommendation
+          t.datetime :locked_until, :deleted_at, :null => true
   end
 
   add_constraint 'request_reviews', 'request_reviews_created_by_id', 'created_by_id', 'users', 'id'
