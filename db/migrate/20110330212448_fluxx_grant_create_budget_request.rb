3393bfe12187ef8556e94b268c063177172ec8b3
Andrew Lee <drew@intramedia.net>
2011-04-06 12:19:31 -1000

    FLX-699 Migration

--- db/migrate/20110330212448_fluxx_grant_create_budget_request.rb
+++ db/migrate/20110330212448_fluxx_grant_create_budget_request.rb
@@ -5,6 +5,7 @@ class FluxxGrantCreateBudgetRequest < ActiveRecord::Migration
       t.integer :created_by_id, :updated_by_id, :null => true, :limit => 12
       t.integer :request_id, :null => false, :limit => 12
       t.integer :amount_requested, :amount_recommended, :null => true, :limit => 12
+      t.string :name, :null => true
       t.datetime :deleted_at,                :null => true
       t.datetime :locked_until,              :null => true
       t.integer :locked_by_id,               :null => true
