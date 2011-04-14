f7d74cd8bcb72ab180c4bd3975e75c59fc2bf4f5
Eric Hansen <ehansen486@gmail.com>
2011-04-13 17:37:38 -0700

    add in a migration to deal with program hierarchy

---
+++ db/migrate/20110414003636_fluxx_grant_migrate_client_stores_to_program_hierarchy.rb
@@ -0,0 +1,23 @@
+class FluxxGrantMigrateClientStoresToProgramHierarchy < ActiveRecord::Migration
+  def self.up
+    
+    # Switch dashboards from the old format to the new format for request hierarchy
+    ClientStore.all.each do |client_store|
+      if client_store.data
+        client_store.data = client_store.data.gsub /\{"name":"request\[sub_program_id\]\[\]","value":"(\d*)"\}/, '{"name":"request[request_hierarchy][]","value":"-\1--"}'
+        client_store.data = client_store.data.gsub /\{"name":"request\[program_id\]\[\]","value":"(\d*)"\}/, '{"name":"request[request_hierarchy][]","value":"\1---"}'
+
+        client_store.data = client_store.data.gsub /\{"name":"request_transaction\[grant_sub_program_ids\]\[\]","value":"(\d*)"\}/, '{"name":"request[request_hierarchy][]","value":"-\1--"}'
+        client_store.data = client_store.data.gsub /\{"name":"request_transaction\[grant_program_ids\]\[\]","value":"(\d*)"\}/, '{"name":"request[request_hierarchy][]","value":"\1---"}'
+
+        client_store.data = client_store.data.gsub /\{"name":"request_report\[grant_sub_program_ids\]\[\]","value":"(\d*)"\}/, '{"name":"request_report[request_hierarchy][]","value":"-\1--"}'
+        client_store.data = client_store.data.gsub /\{"name":"request_report\[grant_program_ids\]\[\]","value":"(\d*)"\}/, '{"name":"request_report[request_hierarchy][]","value":"\1---"}'
+        client_store.save
+      end
+    end
+  end
+
+  def self.down
+    
+  end
+end
