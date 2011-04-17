252a228b8db5fd22659c73961680ff38b28bac13
Eric Hansen <ehansen486@gmail.com>
2011-04-16 20:57:57 -0700

    add hierarchy marker to dashboards

---
+++ db/migrate/20110417035725_fluxx_grant_add_hierarchy_marker_to_existing_dashboards.rb
@@ -0,0 +1,24 @@
+class FluxxGrantAddHierarchyMarkerToExistingDashboards < ActiveRecord::Migration
+  def self.up
+    # Switch dashboards from the old format to the new format for request hierarchy
+    ClientStore.all.each do |client_store|
+      if client_store.data
+        funj = client_store.data.de_json
+        funj['cards'].each do |card|
+          if data_attrs = card['listing']['data']
+            if data_attrs.any?{|element| element.to_json =~ /"name":"request/}
+              data_attrs << {"name" => "request[hierarchies][]", "value" => "request_hierarchy"}
+              data_attrs << {"name" => "request[hierarchies][]", "value" => "allocation_hierarchy"}
+            end
+          end
+        end
+        client_store.data = funj.to_json
+        client_store.save
+      end
+    end
+  end
+
+  def self.down
+    
+  end
+end
