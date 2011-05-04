08744ebbc67fc111e395a9b492ef5196deb1e37b
Andrew Lee <drew@intramedia.net>
2011-05-03 15:03:53 -1000

    FLX-656 Migration for LOIs

---
+++ db/migrate/20110503015250_fluxx_grant_create_loi.rb
@@ -0,0 +1,21 @@
+class FluxxGrantCreateLoi < ActiveRecord::Migration
+  def self.up
+        create_table "lois", :force => true do |t|
+    t.timestamps
+    t.integer :created_by_id, :updated_by_id, :null => true, :limit => 12
+    t.string :applicant, :organization, :project_title, :email, :phone
+    t.text :project_summary
+    t.string :address, :null => true
+    t.integer :program_id, :null => true, :limit => 12
+    t.integer :locked_by_id,               :null => true
+    t.datetime :locked_until, :deleted_at, :null => true
+  end
+
+  add_constraint 'lois', 'lois_created_by_id', 'created_by_id', 'users', 'id'
+  add_constraint 'lois', 'lois_updated_by_id', 'updated_by_id', 'users', 'id'
+  end
+
+  def self.down
+    drop_table "lois"
+  end
+end
