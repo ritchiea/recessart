ae17797d1debbcadc0262d41e6986c611e2675ce
Eric Hansen <ehansen486@gmail.com>
2011-05-31 10:56:27 -0700

    fix populate orig request amendments, add amount conversion migration

--- db/migrate/20110525232619_fluxx_grant_populate_original_request_amendments.rb
+++ db/migrate/20110525232619_fluxx_grant_populate_original_request_amendments.rb
@@ -1,18 +1,42 @@
 class FluxxGrantPopulateOriginalRequestAmendments < ActiveRecord::Migration
+  def self.fill_in_amendment(amendment, changes)
+    amendment.amount_recommended = changes["amount_recommended"] if changes["amount_recomended"]
+          
+    unless (FLUXX_CONFIGURATION[:dont_use_duration_in_requests])
+      amendment.duration = changes["duration_in_months"] if changes["duration_in_months"]
+    else
+      amendment.start_at = changes["grant_begins_at"] if changes["grant_begins_at"]
+      amendment.end_at = changes["grant_closed_at"] if changes["grant_closed_at"]
+    end
+
+    return !!(changes["grant_begins_at"] || changes["grant_closed_at"] || 
+      changes["duration_in_months"] || changes["amount_recommended"])
+  end
+
   def self.up
-    Request.where(:granted => true).each { |r|
-      amendment = r.request_amendments.build(
-        :original => true,
-        :amount_recommended => r.amount_recommended,
-        :start_date => r.grant_begins_at,
-        :end_date => r.grant_closed_at,
-        :duration => r.duration_in_months
-      )
-      amendment.save
+    RequestAmendment.delete_all
+    Request.where(:granted => 1).each { |request|
+      original = request.request_amendments.build(:original => true)
+      fill_in_amendment(original, request.attributes);
+
+      request.audits.each { |audit|
+        changes = audit.audit_changes
+        changes.each {|x,v| changes[x] = v.last if v && v.is_a?(Array) }
+
+        if original.new_record?
+          fill_in_amendment(original, changes)
+          original.save if state = changes["state"] and state =~ /(granted|closed|rejected)/i
+        else
+          amendment = request.request_amendments.build(:original => false)
+          amendment.save if fill_in_amendment(amendment, changes)
+        end
+      }
+
+      original.save if original.new_record?
     }
   end
 
   def self.down
-    RequestAmendment.where(:original => true).destroy
+    RequestAmendment.delete_all
   end
 end
