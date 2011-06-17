ca3f27971fcb26acfd56bbfd0c541abc80972d12
Eric Hansen <ehansen486@gmail.com>
2011-06-17 16:06:30 -0700

    delete extraneous migration

--- db/migrate/20110617185003_fluxx_crm_add_is_grantor_to_organizations.rb
+++
@@ -1,9 +0,0 @@
-class FluxxGrantAddIsGrantorToOrganizations < ActiveRecord::Migration
-  def self.up
-    add_column :organizations, :is_grantor, :boolean, :default => false
-  end
-
-  def self.down
-    remove_column :organizations, :is_grantor
-  end
-end
