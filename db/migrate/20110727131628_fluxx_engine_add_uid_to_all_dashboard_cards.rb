9a7a9208df8adfc5bb330e59aa0f9f76d3b26a1f
Eric Hansen <ehansen486@gmail.com>
2011-07-27 15:45:39 +0200

    fix script

--- db/migrate/20110727131628_fluxx_engine_add_uid_to_all_dashboard_cards.rb
+++ db/migrate/20110727131628_fluxx_engine_add_uid_to_all_dashboard_cards.rb
@@ -1,14 +1,18 @@
 class FluxxEngineAddUidToAllDashboardCards < ActiveRecord::Migration
   def self.up
     ClientStore.where(:client_store_type => 'dashboard').all.each do |dashboard|
-      funj = dashboard.data.de_json
-      cards = funj['cards']
-      funj['nextUid'] = cards.size + 1;
-      cards.each_with_index do |card, i|
-        card['uid'] = i + 1
+      if dashboard && dashboard.data
+        funj = dashboard.data.de_json
+        cards = funj['cards']
+        if cards
+          funj['nextUid'] = cards.size + 1;
+          cards.each_with_index do |card, i|
+            card['uid'] = i + 1
+          end
+          dashboard.data = funj.to_json
+          dashboard.save
+        end
       end
-      dashboard.data = funj.to_json
-      dashboard.save
     end
   end
 
