feded661ecc8d88d0fa73181f6c6ae628e76acaa
Eric Hansen <ehansen486@gmail.com>
2011-10-31 14:03:41 -0700

    add indices

--- db/migrate/20111031193721_fluxx_crm_add_model_document_document_type_field.rb
+++ db/migrate/20111031193721_fluxx_crm_add_model_document_document_type_field.rb
@@ -1,13 +1,16 @@
 class FluxxCrmAddModelDocumentDocumentTypeField < ActiveRecord::Migration
   def self.up
     change_table :model_documents do |t|
-      t.string :label, :null => false, :default => 'default'
+      t.string :doc_label, :null => false, :default => 'default'
     end
+    
+    add_index :model_documents, :doc_label
+    add_index :model_documents, [:documentable_type, :documentable_id, :doc_label], :name => 'mod_docs_type_docid_label'
   end
 
   def self.down
     change_table :model_documents do |t|
-      t.remove :label
+      t.remove :doc_label
     end
   end
 end
