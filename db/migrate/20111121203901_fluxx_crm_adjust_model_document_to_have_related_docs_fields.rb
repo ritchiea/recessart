54541863c04ee6cd903af3cfdfc74ea6a5eac1be
Eric Hansen <ehansen486@gmail.com>
2011-11-22 11:08:13 -0800

    change name of column

--- db/migrate/20111121203901_fluxx_crm_adjust_model_document_to_have_related_docs_fields.rb
+++ db/migrate/20111121203901_fluxx_crm_adjust_model_document_to_have_related_docs_fields.rb
@@ -2,10 +2,10 @@ class FluxxCrmAdjustModelDocumentToHaveRelatedDocsFields < ActiveRecord::Migrati
   def self.up
     change_table :model_document_templates do |t|
       t.string :disposition
-      t.integer :related_model_document_id
+      t.integer :related_model_document_template_id
     end
     
-    add_constraint 'model_document_templates', 'mdt_related_model_document_id', 'related_model_document_id', 'model_document_templates', 'id'
+    add_constraint 'model_document_templates', 'mdt_related_model_doc_templt_id', 'related_model_document_template_id', 'model_document_templates', 'id'
   end
 
   def self.down
